a8cd194ae8d14fe9deeadfde3a4a3ae9349d6053
jeongleo <leo084080@naver.com>
2021-07-18 10:33:28 +0000

    make blog post detail page with CBV

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    # CBV 구현
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+    
+    # FBV 구현
+    # path('', views.index),
+    # path('<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,43 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+# CBV 구현
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
     
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+    ## ListView는 object_list라는 이름을 가진 템플릿 파일을 쓴다.
+    ## 다른 이름을 쓰려면 아래와 같이 선언해주어야 한다.
+    # template_name = 'blog/index.html' 
+
+class PostDetail(DetailView):
+    model = Post
+
+## FBV 구현
+
+#from django.shortcuts import render
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+    
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
+
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+    
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
